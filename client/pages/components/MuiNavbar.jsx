23249bb2f4582cf02caf533ecf86805a4c103cc0
Juan Briones <JuanMBriones@outlook.com>
2022-10-17 20:45:36 -0500

    feat: Add NavBar support

--- client/pages/components/MuiNavbar.jsx
+++ client/pages/components/MuiNavbar.jsx
@@ -6,14 +6,30 @@ import {
   Typography,
   Stack,
   Button,
+  Menu,
+  MenuItem,
+  Link,
 } from '@mui/material';
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+import {useState} from 'react';
 
 /**
  * @name MuiNavbar
  * @return {React.Component}
  */
 export default function MuiNavbar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar
       position="static"
@@ -41,14 +57,63 @@ export default function MuiNavbar() {
           <Button
             color='inherit'
           >
-            Features
+            <Link
+              href='/'
+              underline='none'
+              color='inherit'
+            >
+              Home
+            </Link>
+          </Button>
+          <Button
+            color='inherit'
+            id='resources-button'
+            onClick={handleClick}
+            aria-control={open ? 'resources-menu' : undefined }
+            aria-haspopup='true'
+            aria-expanded={open ? 'true' : undefined}
+            endIcon={<KeyboardArrowDownIcon />}
+          >
+            <Link
+              href='/resources'
+              underline='none'
+              color='inherit'
+            >
+              Resources
+            </Link>
           </Button>
           <Button
             color='inherit'
           >
-            Login
+            <Link
+              href='/login'
+              underline='none'
+              color='inherit'
+            >
+              Login
+            </Link>
           </Button>
         </Stack>
+        <Menu
+          id='resources-menu'
+          anchorEl={anchorEl}
+          open={open}
+          MenuListProps={{
+            'aria-labelledby': 'resourced-button',
+          }}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          <MenuItem onClick={handleClose}>Blog</MenuItem>
+          <MenuItem onClick={handleClose}>Podcast</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
