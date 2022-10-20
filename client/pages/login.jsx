b35c58b50d6f337194fdff65cfef13c5b0a03892
Juan Briones <JuanMBriones@outlook.com>
2022-10-20 00:38:16 -0500

    feat: Restricted login only for Tec accounts

--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -20,6 +20,7 @@ export default function Login() {
         clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
       >
         <GoogleLogin
+          hosted_domain={'tec.mx'}
           onSuccess={async (credentialResponse) =>{
             console.log(credentialResponse);
 
