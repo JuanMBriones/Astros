2c900bb570517921e0f40e539b519f5e0e1ec126
Juan Briones <JuanMBriones@outlook.com>
2022-10-20 01:22:38 -0500

    Testing prod env varibles on deployment

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(require('cors')());
 
-// initialize db.
+// initialize db
 dbConfig();
 
 app.get('/', (req, res) => {
