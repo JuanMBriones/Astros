558c5e8f22494ed4f90e36ed511777853ab32b6e
Juan Briones <JuanMBriones@outlook.com>
2022-10-23 17:00:31 -0500

    feat: Add footer

---
+++ client/pages/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/**
+ * @return {Component}
+ */
+export default function Footer() {
+  return (
+    // align items to the center
+    <footer>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <p>
+          <strong>Sistema de Inscriptiones+</strong> by{' '}
+          <a href="httpw://www.tec.mx">ITESM</a>.
+        </p>
+      </div>
+    </footer>
+  );
+}
