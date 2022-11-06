c27ebc3094a99d65ea9ae088719992ae33822969
Juan Briones <JuanMBriones@outlook.com>
2022-11-05 20:09:58 -0600

    feat: Add endpoints for user roles

--- server/controllers/Profesor/index.js
+++ server/controllers/Profesor/index.js
@@ -10,6 +10,14 @@ router.get('/profesores', asyncHandler(profesorCtr.getAll()));
 
 router.get('/profe', asyncHandler(profesorCtr.getDataProfe()));
 
+router.post('/profe', asyncHandler(profesorCtr.postProfesor()));
+
+router.post('/giveAdmin', asyncHandler(profesorCtr.givePower()));
+
+router.post('/quitAdmin', asyncHandler(profesorCtr.nerfProfessor()));
+
+router.post('/isAdmin', asyncHandler(profesorCtr.isAdmin()));
+
 router.get('/profMaterias', asyncHandler(profesorCtr.getProfMaterias()));
 
 router.get('/assignProf', asyncHandler(profesorCtr.assignProf()));
