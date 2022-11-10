c2af0dd98f684f8f2feb442136ccfc4d42428926
Axel Rodríguez <A01570152@itesm.mx>
2022-11-10 02:54:44 -0600

    feat: newClase saves and posts form

--- client/pages/newClase.jsx
+++ client/pages/newClase.jsx
@@ -8,16 +8,44 @@ import Select from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import axios from 'axios';
+// falta cip
 
 /**
  * newClase
  * @return {React.Component}
  */
 export default function newClase() {
+  const objetoClase = {paquete:null, clave:null, grupo:null, nombre:null, edificio:null, salon:null,
+  modalidad:null, tipo:null, modelo:null, carga:null, semestre:null, periodo:null, horario:null, ingles:null};
+  const [objClase, setAtributos] = React.useState(objetoClase);
   const [modalidad, setModalidad] = React.useState('');
   const [tipo, setTipo] = React.useState('');
   const [modelo, setModelo] = React.useState('');
   const [periodo, setPeriodo] = React.useState('');
+
+  const submitForm = () => {
+    axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/clase/add/`,
+      {
+        paquete: objClase.paquete,
+        clave: objClase.clave,
+        grupo: objClase.grupo,
+        nombre: objClase.nombre,
+        edificio: objClase.edificio,
+        salon: objClase.salon,
+        modalidad: objClase.modalidad,
+        tipo: objClase.tipo,
+        modelo: objClase.modelo,
+        carga: objClase.carga,
+        semestre: objClase.semestre,
+        periodo: objClase.periodo,
+        horario: objClase.horario,
+        ingles: objClase.ingles,
+      },
+    ).then((res) => {
+      console.log(res);
+    });
+  };
 
   const handleModalidad = event => {
     setModalidad(event.target.value);
@@ -74,11 +102,13 @@ export default function newClase() {
               label='Paquete'
               variant='outlined'
               sx={{mb: '1rem'}}
+              onChange={(e) => setAtributos({...objClase, paquete: e.target.value})}
             />
             <TextField
               id='outlined-basic'
               label='Clave'
               variant='outlined'
+              onChange={(e) => setAtributos({...objClase, clave: e.target.value})}
             />
             <TextField
               id='outlined-basic'
@@ -89,6 +119,7 @@ export default function newClase() {
                   event.preventDefault();
                 }
               }}
+              onChange={(e) => setAtributos({...objClase, grupo: e.target.value})}
             />
           </Stack>
           <Stack
@@ -103,11 +134,13 @@ export default function newClase() {
               variant='outlined'
               sx={{mb: '1rem'}}
               fullWidth
+              onChange={(e) => setAtributos({...objClase, nombre: e.target.value})}
             />
             <TextField
               id='outlined-basic'
               label='Edificio'
               variant='outlined'
+              onChange={(e) => setAtributos({...objClase, edificio: e.target.value})}
             />
             <TextField
               id='outlined-basic'
@@ -118,6 +151,7 @@ export default function newClase() {
                   event.preventDefault();
                 }
               }}
+              onChange={(e) => setAtributos({...objClase, salon: e.target.value})}
             />
           </Stack>
           <Stack
@@ -131,7 +165,7 @@ export default function newClase() {
               <Select
                 value={modalidad}
                 label="Modalidad"
-                onChange={handleModalidad}
+                onChange={(e) => {handleModalidad(e); setAtributos({...objClase, modalidad: e.target.value});}}
               >
                 <MenuItem value={'Presencial'}>Presencial</MenuItem>
                 <MenuItem value={'MFDL'}>MFDL</MenuItem>
@@ -143,8 +177,8 @@ export default function newClase() {
               <InputLabel>Tipo</InputLabel>
               <Select
                 value={tipo}
-                 label="Tipo"
-                onChange={handleTipo}
+                label="Tipo"
+                onChange={(e) => {handleTipo(e); setAtributos({...objClase, tipo: e.target.value});}}
               >
                 <MenuItem value={'Bloque'}>Bloque</MenuItem>
                 <MenuItem value={'Topico'}>Tópico</MenuItem>
@@ -157,7 +191,7 @@ export default function newClase() {
               <Select
                 value={modelo}
                 label="Modelo"
-                onChange={handleModelo}
+                onChange={(e) => {handleModelo(e); setAtributos({...objClase, modelo: e.target.value});}}
               >
                 <MenuItem value={'Tec 20'}>Tec 20</MenuItem>
                 <MenuItem value={'Tec 21'}>Tec 21</MenuItem>
@@ -179,6 +213,7 @@ export default function newClase() {
                   event.preventDefault();
                 }
               }}
+              onChange={(e) => setAtributos({...objClase, carga: e.target.value})}
             />
             <TextField
               id='outlined-basic'
@@ -189,13 +224,14 @@ export default function newClase() {
                   event.preventDefault();
                 }
               }}
+              onChange={(e) => setAtributos({...objClase, semestre: e.target.value})}
             />
              <FormControl fullWidth>
               <InputLabel >Periodo</InputLabel>
               <Select
                 value={periodo}
                 label="Periodo"
-                onChange={handlePeriodo}
+                onChange={(e) => {handlePeriodo(e); setAtributos({...objClase, periodo: e.target.value});}}
               >
                 <MenuItem value={'PMT1'}>PMT1</MenuItem>
                 <MenuItem value={'PMT2'}>PMT2</MenuItem>
@@ -216,10 +252,19 @@ export default function newClase() {
               id='outlined-basic'
               label='Horario'
               variant='outlined'
-              style = {{width: 400}} 
+              style = {{width: 400}}
+              onChange={(e) => {
+                if (/^(((Lu|Ma|Mi|Ju|Vi)+ ([01]?[0-9]|2[0-3]):[0-5][0-9])(\/)*)+$/.test(e.target.value)) {
+                  e.target.style.color = 'green';
+                  setAtributos({...objClase, horario: e.target.value});
+                }
+                else{
+                  e.target.style.color = 'red';
+                }
+              }}
             />
             <FormGroup>
-                <FormControlLabel control={<Checkbox />} label={
+                <FormControlLabel control={<Checkbox onChange={(e) => setAtributos({...objClase, ingles: e.target.checked})}/>} label={
                   <Typography variant="body1" gutterBottom>
                     Inglés
                   </Typography> }/>
@@ -228,6 +273,7 @@ export default function newClase() {
           <Button
             variant='contained'
             color='success'
+            onClick={submitForm}
           >
             Crear
           </Button>
