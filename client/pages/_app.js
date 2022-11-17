cce2389e68049864038ac91936b2202246de8111
Juan Briones <JuanMBriones@outlook.com>
2022-11-17 04:31:47 -0600

    feat: Add navbar restrictions

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -9,6 +9,15 @@ import AnimatedNav from './components/NavBar/AnimatedNav';
 import axios from 'axios';
 
 /**
+   * getSanitizedPath - Sanitizes the path to remove the query params
+   * @param {*} urlPath
+   * @return {String}
+   */
+function getSanitizedPath(urlPath) {
+  return urlPath.split('#')[0].split('?')[0];
+}
+
+/**
  *
  * @param {Object} Component to be rendered
  * @param {Object} pageProps to be passed to the component
@@ -18,7 +27,7 @@ function MyApp({Component, pageProps}) {
   const [hideNavbarFooter, setHideNavbarFooter] = useState(false);
   // eslint-disable-next-line no-unused-vars
   const [user, setUser] = useState(undefined);
-  // const [isAdmin, setIsAdmin] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
   const barLabels = ['Home', 'About', 'Contact', 'Blog', 'Support'];
   const backMenu = {
     'color': 'orange',
@@ -27,42 +36,19 @@ function MyApp({Component, pageProps}) {
     },
   };
 
-  useEffect(() => {
-    const profInfo = localStorage.getItem('professor');
-    if (profInfo) {
-      const profInfoJson = JSON.parse(profInfo);
-      axios({
-        method: 'post',
-        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/isAdmin/`,
-        data: {
-          nomina: profInfoJson.profe.nomina,
-        },
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }).then((res) => {
-        if (res.data.message === 'Profesor is admin') {
-          setIsAdmin(true);
-        } else {
-          setIsAdmin(false);
-        }
-      }).catch((err) => {
-        console.log(err);
-      });
-    } else {
-      console.log('not logged in');
-    }
-  }, []);
-
-  // TODO: Refactor this to use a context
   let defaultNavInfo = {
     'Home': {
       'condition': 1,
       'url': '/',
       'color': 'orange',
     },
+    'Mi Horario': {
+      'condition': isAdmin? 0 : 1,
+      'url': '/views/Horario',
+      'color': 'orange',
+    },
     'Profesores': {
-      'condition': 1,
+      'condition': isAdmin? 1 : 0,
       'color': 'orange',
       'children': {
         'Horario & clases': {
@@ -70,10 +56,12 @@ function MyApp({Component, pageProps}) {
           'color': 'orange',
         },
         'Agregar profesores': {
+          'condition': isAdmin? 1 : 0,
           'url': '/newProfe',
           'color': 'orange',
         },
         'Agregar profesores con archivo': {
+          'condition': isAdmin? 1 : 0,
           'url': '/uploadProfe',
           'color': 'orange',
         },
@@ -84,7 +72,7 @@ function MyApp({Component, pageProps}) {
       },
     },
     'Clases': {
-      'condition': 1,
+      'condition': isAdmin? 1 : 0,
       'children': {
         'Asignar Clases': {
           'url': '/views/Clases',
@@ -106,11 +94,48 @@ function MyApp({Component, pageProps}) {
       'color': 'blue',
     },
     'Logout': {
-      'condition': 1,
+      'condition': getSanitizedPath(useRouter().asPath) !== '/login',
       'url': '/login',
       'color': 'red',
     },
   };
+
+  useEffect(() => {
+    const profInfo = localStorage.getItem('professor');
+    if (profInfo) {
+      const profInfoJson = JSON.parse(profInfo);
+      axios({
+        method: 'post',
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/isAdmin/`,
+        data: {
+          nomina: profInfoJson.profe.nomina,
+        },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then((res) => {
+        if (res.data.message === 'Profesor is admin') {
+          setIsAdmin(true);
+          console.log('POWER🤑');
+
+          console.log(navInfo);
+          const newNavInfo = navInfo;
+          newNavInfo['Clases']['condition'] = isAdmin? 1 : 0;
+          newNavInfo['Profesores']['condition'] = isAdmin? 1 : 0;
+          newNavInfo['Mi Horario']['condition'] = isAdmin? 0 : 1;
+
+          console.log(newNavInfo);
+          setNavInfo(newNavInfo);
+        } else {
+          setIsAdmin(false);
+        }
+      }).catch((err) => {
+        console.log(err);
+      });
+    } else {
+      console.log('not logged in');
+    }
+  }, []);
 
   const [navInfo, setNavInfo] = useState(defaultNavInfo);
 
@@ -118,9 +143,9 @@ function MyApp({Component, pageProps}) {
   // barLabels.forEach((key, i) => (navInfo[key] = colors[i]));
 
   useEffect(() => {
-    const rawUrl = asPath.split('#')[0].split('?');
-    console.log(rawUrl[0]);
-    if (rawUrl[0] === '/') {
+    console.log('asPath', asPath);
+    const rawUrl = getSanitizedPath(asPath);
+    if (rawUrl === '/') {
       setHideNavbarFooter(true);
     }
     setUser('test');
